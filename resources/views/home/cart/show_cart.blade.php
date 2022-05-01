4ce2f3a6bef4bd17366a32e5352ece31c9e8156e
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-05-01 10:23:33 +0700

    complete ajax + loading ajax

--- resources/views/home/cart/show_cart.blade.php
+++ resources/views/home/cart/show_cart.blade.php
@@ -7,63 +7,67 @@
 @section('js')
     <script src="clients/detailsSanPham/cart.js"></script>
     <script type="text/javascript">
+      $(document).ready(function () {       
+        $('.upCart').on('change keyup', function(e){
+          var target = e.currentTarget;
+          var newqty = $(target).val();
+          var rowId = $(target).nextUntil("input").last().next().val();
 
-      // function updateCart(qty, rowId) {
-      //   $.get(
-      //     '{{ asset('/update') }}',
-      //     {qty: qty,rowId:rowId },
-      //     function () {
-      //       location.reload();
-      //     }
-      //   );
-      // }
-
-      function addQty(rowId) {
-        const qty = (parseInt)($('.number-product').val()) + 1;
-        console.log(typeof qty);
-        console.log(qty);
-        $.get(
-          '{{ asset('/update') }}',
-          {qty: qty,rowId:rowId },
-          function () {
-            location.reload();
+          if(newqty <=0){ alert('enter only valid qty') }
+          else {
+            $.ajax({
+                type: 'get',
+                dataType: 'html',
+                url: '<?php echo url('/update');?>/'+rowId,
+                data: "qty=" + newqty + "& rowId=" + rowId,
+                beforeSend: () => {
+                  $('.loading-ajax').removeClass('hidden');
+                },
+                success: function (response) {
+                  $('#updateDiv').html(response);
+                  $('.loading-ajax').addClass('hidden');
+                }
+            });
           }
-        );
-      }
-
-      function minusQty(rowId) {
-        const qty = (parseInt)($('.number-product').val()) - 1;
-        $.get(
-          '{{ asset('/update') }}',
-          {qty: qty,rowId:rowId },
-          function () {
-            location.reload();
-          }
-        );
-      }
-
-      // ----------------
-      $(document).ready(function () {
-        <?php for($i=1;$i<20;$i++){?>
-          $('#upCart<?php echo $i;?>').on('change keyup', function(){
-            var newqty = $('#upCart<?php echo $i;?>').val();
-            var rowId = $('#rowId<?php echo $i;?>').val();
-
-            if(newqty <=0){ alert('enter only valid qty') }
-            else {
-              $.ajax({
-                  type: 'get',
-                  dataType: 'html',
-                  url: '<?php echo url('/update');?>/'+rowId,
-                  data: "qty=" + newqty + "& rowId=" + rowId,
-                  success: function (response) {
-                      // console.log(response);
-                      $('#updateDiv').html(response);
-                  }
-              });
+        });    
+        
+        $('.button-minus').on('click', function (e) {
+          var target = e.currentTarget;
+          var newqty = $(target).next().val();
+          var rowId = $(target).nextUntil("input:hidden").last().next().val();
+          $.ajax({
+            type: 'get',
+            dataType: 'html',
+            url: '<?php echo url('/update');?>/'+rowId,
+            data: "qty=" + newqty + "& rowId=" + rowId,
+            beforeSend: () => {
+              $('.loading-ajax').removeClass('hidden');
+            },
+            success: function (response) {
+              $('#updateDiv').html(response);
+              $('.loading-ajax').addClass('hidden');
             }
-          });
-        <?php } ?>
+            });
+          });  
+        
+        $('.button-add').on('click', function (e) {
+          var target = e.currentTarget;
+          var newqty = $(target).prev().val();
+          var rowId = $(target).next().val();
+          $.ajax({
+            type: 'get',
+            dataType: 'html',
+            url: '<?php echo url('/update');?>/'+rowId,
+            data: "qty=" + newqty + "& rowId=" + rowId,
+            beforeSend: () => {
+              $('.loading-ajax').removeClass('hidden');
+            },
+            success: function (response) {
+              $('#updateDiv').html(response);
+              $('.loading-ajax').addClass('hidden');
+            }
+            });
+          }); 
         const qtyProduct = $('.qty-product').text();
         $('.badge').text(qtyProduct);
       });
@@ -96,7 +100,6 @@
             </tr>
           </thead>
           <tbody>
-            <?php $count =1;?>
             @foreach($content as $v_content)
             <tr class="js-product-count">
               <td>
@@ -117,7 +120,7 @@
                 <div>
                   <form action="{{ URL::to('/update-cart-quantity') }}" method="post">
                     {{ csrf_field() }}
-                  <a class="button-count" onclick="minusQty('{{ $v_content->rowId }}')">
+                  <a class="button-count button-minus">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       class="inline-flex w-6 h-6 text-red-600"
@@ -139,11 +142,9 @@
                     value="{{ $v_content->qty }}"
                     min="1"
                     data-dvt="2"
-            
-                    id="upCart<?php echo $count;?>"
-                    class="number-product w-12 text-center bg-gray-100 outline-none"
+                    class="upCart number-product w-12 text-center bg-gray-100 outline-none"
                   />
-                  <a class="button-count" onclick="addQty('{{ $v_content->rowId }}')">
+                  <a class="button-count button-add">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       class="inline-flex w-6 h-6 text-green-600"
@@ -159,8 +160,78 @@
                       />
                     </svg>
                   </a>
-                  <input type="hidden" id="rowId<?php echo $count;?>"  value="{{ $v_content->rowId }}" name="rowId_cart">
+                  <input type="hidden" class="rowId" value="{{ $v_content->rowId }}" name="rowId_cart">
                   </form>
+                  <div class="loading-ajax loading hidden fixed inset-0 z-9999">
+                    <div class="absolute inset-0 bg-gray-500 opacity-50 z-a-1"></div>
+                    <div div class="flex justify-center items-center w-full h-full">
+                        <svg class="lds-spinner" width="54px"  height="54px"
+                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100"
+                        preserveAspectRatio="xMidYMid" style="background: none;">
+                        <g transform="rotate(0 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(30 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(60 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.75s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(90 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(120 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(150 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.5s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(180 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(210 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(240 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.25s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(270 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(300 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g><g transform="rotate(330 50 50)">
+                            <rect x="47" y="24" rx="9.4" ry="4.8" width="6" height="12" fill="#f5490f">
+                            <animate attributeName="opacity" values="1;0" times="0;1" dur="1s"
+                            begin="0s" repeatCount="indefinite"></animate>
+                            </rect>
+                        </g>
+                        </svg>
+                    </div>
+                  </div>
                 </div>
               </td>
               <td class="p-4 px-6 text-center whitespace-nowrap">{{ $v_content->qty }} x {{ number_format($v_content->price,0, ',', '.') }}</td>
@@ -189,7 +260,6 @@
                 </a>
               </td>
             </tr>
-            <?php $count++;?>
             @endforeach
       
           </tbody>
