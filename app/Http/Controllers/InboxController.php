157c7f405b4c2e675b4e49e313a89c431f33c932
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-06-21 18:52:24 +0700

    #none debug livewire

--- app/Http/Controllers/InboxController.php
+++ app/Http/Controllers/InboxController.php
@@ -63,6 +63,7 @@ class InboxController extends Controller
         if((Auth::user() != null)) {
             $temp = $adminLogin;
             for ($i = 0; $i < count($temp); $i++) {
+                dd($adminLogin);
                 if($adminLogin[$i]['email'] == Auth::user()->email) {
                     $adminLogin = $adminLogin[$i];
                 }
