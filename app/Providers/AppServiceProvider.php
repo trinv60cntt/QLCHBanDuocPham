6cf17b23429324a633f578657cabf83c0f259888
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-06-21 18:48:49 +0700

    #none hosting heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \URL::forceScheme('https');
+        $this->app->bind('path.public', function() {
+            // return your own correct path.
+            return realpath(base_path('../'));
+       });
         Schema::defaultStringLength(191);
     }
 }
