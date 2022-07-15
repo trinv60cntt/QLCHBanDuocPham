efcbb9eb952c0f0d8f4e8b3ec594c6ca78d9dae6
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-07-16 00:23:56 +0700

    1

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
+        'sqlite' => [ 
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
