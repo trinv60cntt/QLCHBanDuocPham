0950e405b14ff1407b3f9490d87e6019d5042067
Nguyễn Văn Trí <91021009+trinv60cntt@users.noreply.github.com>
2022-07-16 22:19:42 +0700

    #none AdminQuyen

--- app/Http/Controllers/AdminQuyenController.php
+++ app/Http/Controllers/AdminQuyenController.php
@@ -71,7 +71,7 @@ class AdminQuyenController extends Controller
     Quyen::create([
       'tenQuyen' => $request->tenQuyen,
       'moTa' => $request->moTa,
-      'parent_id' => 40,
+      'parent_id' => $quyen->quyen_id,
     ]);
 
     return redirect()->route('quyens.index')->with('success', 'Thêm mới quyền thành công');
